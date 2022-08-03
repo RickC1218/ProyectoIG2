318b481c02d81dd95761fcf0d81c58fe6e925f85
RickC1218 <ricardo.erazo@epn.edu.ec>
2022-08-02 21:30:01 -0500

    Cierre de while en estrenos.php

--- estrenos.php
+++ estrenos.php
@@ -29,33 +29,33 @@
                     </div>
                     <div class="col-4">
                         <button id="menu" class="col align-self-end navbar-toggler barra navbar-light" type="button" data-bs-toggle="collapse" 
-                        data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" 
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse col align-self-end barra" id="navbarContent">
-                        <ul class="navbar-nav m-auto col-12">
-                            <li class="nav-item">
-                                <a class="nav-link" id="boton" href="/index.php">Ingresar</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/estrenos.php">Estrenos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/dulceria.php">Dulcería</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/perfil.php">Perfil</a>
-                            </li>
-                            <li class="nav-item">  
-                                <a class="nav-link" href="/carrito.php" id="boton">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart4 " viewBox="0 0 16 16">
-                                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-                                    </svg>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                            data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" 
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse col align-self-end barra" id="navbarContent">
+                            <ul class="navbar-nav m-auto col-12">
+                                <li class="nav-item">
+                                    <a class="nav-link" id="boton" href="/index.php">Ingresar</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/estrenos.php">Estrenos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/dulceria.php">Dulcería</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/perfil.php">Perfil</a>
+                                </li>
+                                <li class="nav-item">  
+                                    <a class="nav-link" href="/carrito.php" id="boton">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart4 " viewBox="0 0 16 16">
+                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
@@ -81,6 +81,9 @@
                         <h6><?php echo $mostrar['DURACION_PELICULA'] ?></h6>
                     </div>
                 </div>
+                <?php
+                    }
+                ?>
             </div>
         </section>
     </div>
