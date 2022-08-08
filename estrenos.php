ab5fc5be5d50e0ba23f9c953b62e750deb840c9f
RickC1218 <ricardoerazoliga@gmail.com>
2022-08-08 12:52:13 -0500

    arreglo estreno.php

--- estrenos.php
+++ estrenos.php
@@ -16,47 +16,53 @@ $conexion = mysqli_connect('localhost', 'root', 'admin', 'proyecto_cine');
 </head>
 
 <body>
-    <header>
-        <div class="container-fluid">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid" id="cabecera">
-                    <div class="col align-self-center col-1 logo">
-                        <a class="navbar-brand" href="/index.html">
-                            <span><img src="/recursos/imagenes/logo-TopCine.png" alt="logo" style="width: 100%;"></span>
-                        </a>
-                    </div>
-                    <div class="col-4">
-                        <button id="menu" class="col align-self-end navbar-toggler barra navbar-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse col align-self-end barra" id="navbarContent">
-                            <ul class="navbar-nav m-auto col-12">
-                                <li class="nav-item">
-                                    <a class="nav-link" id="boton" href="/index.php">Ingresar</span></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/estrenos.php">Estrenos</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/dulceria.php">Dulcería</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/perfil.php">Perfil</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/carrito.php" id="boton">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart4 " viewBox="0 0 16 16">
-                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+    <!--Inicio Header-->
+    <header id="header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <div id="logo">
+                    <a class="navbar-brand" href="#"><img class="img-fluid" src="recursos/imagenes/logo-TopCine.png"
+                            alt="Logo TopCine" title="Logo TopCine"></a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#MenuDespegable"
+                    aria-controls="MenuDespegable" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="MenuDespegable">
+                    <div class="col">
+                        <ul class="navbar-nav justify-content-end">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.html" id="login"><b>Ingresar</b></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link barra-opciones" href="#Productos"><b>Inicio</b></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link barra-opciones" href="#Marcas"><b>Cartelera</b></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link barra-opciones" href="#Sucursales"><strong>Dulcería</strong></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link barra-opciones" href="#Contacto"><strong>Perfil</strong></a>
+                            </li>
+                            <li class="nav-item carrito">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    class="bi bi-cart4 text-warning" viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                                </svg>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     </header>
+    <!--Fin Header-->
+
+    <div class="clearfix"></div>
+    <!--Inicio Contenido-->
 
     <div class="wrap container-fluid">
         <section id="peliculas" class="col-12">
