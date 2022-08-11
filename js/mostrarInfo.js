4ab1dd3b2b95904be2f035222e5f36b96be356c1
RickC1218 <ricardoerazoliga@gmail.com>
2022-08-10 20:06:35 -0500

    finaliza front-end

---
+++ js/mostrarInfo.js
@@ -0,0 +1,30 @@
+'use strict'
+window.addEventListener('load', () => {
+    var datosCliente = document.querySelector('#editProfile');
+    var historialCliente = document.querySelector('#showRecord');
+
+    var cliente = $('#editarInfo');
+    var historial = $('#pedidos');
+
+    var cajaCli = $('.info-perfil');
+    var cajaHis = $('.info-pedidos');
+
+    cliente.hide();
+    historial.show();
+    cajaHis.css("border", "4px solid white").css("margin", "1%").css("border-radius","15px");
+
+    datosCliente.addEventListener('click', () => {
+        historial.hide();
+        cliente.show();
+        cajaHis.css("border", "").css("margin", "").css("border-radius","");
+        cajaCli.css("border", "4px solid white").css("margin", "1%").css("border-radius","15px");
+    });
+
+    historialCliente.addEventListener('click', () => {
+        cliente.hide();
+        historial.show();
+        cajaHis.css("border", "4px solid white").css("margin", "1%").css("border-radius","15px");
+        cajaCli.css("border", "").css("margin", "").css("border-radius","");
+    });
+
+});
