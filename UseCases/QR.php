edc0256b74bf079283a57777adce2b86e58f43b2
Leo_Andrade <leomijail@hotmail.com>
2022-08-26 21:07:54 -0500

    QR

--- UseCases/QR.php
+++ UseCases/QR.php
@@ -1,4 +1,26 @@
 <?php
+
+require ('../Entities/conexion.php');
+
+date_default_timezone_set("America/Mexico_City");
+$Date = date('Y-m-d');
+
+$numced= 1726639410;
+
+    $sql2 = "SELECT NOMBRE_CLI,titulo_pelicula, NUMBOLETOS_DETPEL, FECHCOMP_FACTURA, VALTOTAL_FACTURA
+    from CLIENTE, FACTURA, DETALLE_PELICULA, PELICULA
+    where CLIENTE.NUMCED_CLI=  $numced
+        and FACTURA.NUMCED_CLI = $numced
+        and FECHCOMP_FACTURA = '$Date'
+        and FACTURA.ID_FACTURA = DETALLE_PELICULA.ID_FACTURA
+        and DETALLE_PELICULA.ID_DETPEL = PELICULA .ID_PELICULA;";
+    $result = $conexion->query($sql2);
+    $resultado2 = $result->fetchAll(PDO::FETCH_ASSOC);
+    
+    $salida = 'Titulo Pelicula: '.$resultado2[0]['titulo_pelicula'].' Numero Boletos: '.$resultado2[0]['NUMBOLETOS_DETPEL'].' Fecha: '.$resultado2[0]['FECHCOMP_FACTURA'].' Valor: '.$resultado2[0]['VALTOTAL_FACTURA'];
+
+
+
 // include QR_BarCode class
 include "QR_BarCode.php";
 
@@ -6,11 +28,17 @@ include "QR_BarCode.php";
 $qr = new QR_BarCode();
 
 // create text QR code
-$qr->text('Prueba 1');
+$qr->text($salida);
+
 
 // save QR code
 
-$qr->qrCode(350,'../recursos/QR/name.png');
+$name = $resultado2[0]['NOMBRE_CLI'];
+$path = '../resources/QR/'.$name.'.png';
+
+
+
+$qr->qrCode(350,$path);
 
 // show QR code
 $qr->qrCode();
